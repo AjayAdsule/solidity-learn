1d7605e02499de99c3bab4e3149b3e8a56d48d4c
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2024-07-28 19:01:10 +0530

    make interface payable function

--- oops/interface.sol
+++ oops/interface.sol
@@ -4,17 +4,33 @@ pragma solidity >=0.8.20;
 interface IfirstInterface {
     // function getData() external view returns(uint);
     function greet() external pure returns (string memory);
+
+    function addEth() external payable;
+
+    function getContractBalance() external view returns (uint256);
 }
 
 contract Interface is IfirstInterface {
     function greet() external pure returns (string memory) {
         return "hello interfaces";
     }
+
+    function addEth() external payable {}
+
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
+    }
 }
 
 contract secondInterface is IfirstInterface {
     function greet() external pure returns (string memory) {
         return "second interface";
+    }
+
+    function addEth() external payable {}
+
+    function getContractBalance() external view returns (uint256) {
+        return address(this).balance;
     }
 }
 
@@ -24,7 +40,14 @@ contract CallerContract {
         pure
         returns (string memory)
     {
-        
         return IfirstInterface(_caller).greet();
+    }
+
+    function getAddressBalance(address _caller)
+        external
+        view
+        returns (uint256)
+    {
+        return IfirstInterface(_caller).getContractBalance();
     }
 }
