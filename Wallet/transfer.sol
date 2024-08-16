2c9ddb57b47d2526fad859b7fe733e248928b328
Ajay A Adsule <103304466+AjayAdsule@users.noreply.github.com>
2024-08-16 23:34:59 +0530

    learn how transfer work in solidity

---
+++ Wallet/transfer.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TypesOfTranfer {
+    // transfer function
+
+    function transferEth(address payable _add, uint256 _value) external {
+        _add.transfer(_value);
+    }
+
+    function transferViaCall(address payable _add) external payable {
+        (bool success, ) = _add.call{value: msg.value}("");
+        if (!success) revert();
+    }
+
+    function transferViaOwner(address payable _add) external payable {
+        _add.transfer(msg.value);
+    }
+
+    function getEth() external payable {}
+}
